f0f45ca2d14678392d1c96bfeff672630c687d46
kaj-kwok <94345485+kaj-kwok@users.noreply.github.com>
2022-01-07 12:42:04 -0800

    linked input form to create objects

--- script.js
+++ script.js
@@ -1,27 +1,77 @@
 //set up constructor
-function book(title, author, pages, read) {
+function books(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.isRead = read;
 }
 
+function addBooktoLibrary(title, author, pages, read) {
+    book = new books(title, author, pages, read) 
+    return myLibrary.push(book);
+}
+
 const myLibrary = [ 
-    {   title: "The Lord of the Rings",
+    {book: {
+        title: "The Lord of the Rings",
         author: "J.R.R. Tolkien",
         pages: 229,
-        isRead: "read",
+        isRead: "true",
+    }
     },
-    {
+    {book: {
         title: "The Hobbit",
         author: "J.R.R. Tolkien",
         pages: 100,
-        isRead: "notRead",
+        isRead: "false",
+    }
     },
 ];
 
-/// modal section
-document.querySelector('#add-book').addEventListener('click', () => {
-    document.querySelector('.bg-modal').style.display = 'flex';
+//capture input
+const formInp = document.querySelector('.form')
+formInp.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const title = formInp.elements.namedItem('title').value
+    const author = formInp.elements.namedItem('author').value
+    const pages = formInp.elements.namedItem('pages').value
+    const read = formInp.elements.namedItem('status').value
+    addBooktoLibrary(title, author, pages, read);
+    closeModal()
+    formInp.reset()
 })
 
+
+/// modal section
+///eventlistener to open modal
+
+const openModalButton = document.querySelector('[data-modal-target]');
+const closeModalButton = document.querySelector('[data-close-button]');
+const overlay = document.getElementById('overlay');
+
+openModalButton.addEventListener('click', () => {
+    const modal = document.querySelector('#modal')
+    openModal(modal)
+})
+
+closeModalButton.addEventListener('click', () => {
+    const modal = document.querySelector('#modal')
+    closeModal(modal)
+})
+
+function openModal(model) {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModal(model) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal')
+    closeModal(modals)
+})
